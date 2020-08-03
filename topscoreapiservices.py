4a476bfd360420a3bdb85cc3746cc4ea17302b9e
BuildTools <unconfigured@null.spigotmc.org>
2020-08-03 12:00:43 +1200

    pushing all changes

--- topscoreapiservices.py
+++ topscoreapiservices.py
@@ -4,6 +4,7 @@ import wumodel
 from topscoreoauth2 import generateAuthToken
 from topscoreoauth2 import collectUserAPIInfo
 import bs4
+import datetime
 
 def collectPlayerID(auth_key):
     url = "https://wds.usetopscore.com/api/me"
@@ -33,9 +34,8 @@ def collectPlayerTeams(playerid, auth_key):
         out.append(wumodel.Team(name, id, img))
     return out
 
-def collectTeamGameInfo(teamid, auth_key):
-    """Return list of game objects for each upcoming game for teamid"""
-    url = "https://wds.usetopscore.com/api/games?team_id={0}&active_events_only=true".format(teamid)
+def collectTeamEvents(teamid, auth_key):
+    url = "https://wds.usetopscore.com/api/events?team_id={}&order_by=date_desc".format(teamid)
 
     headers = {
         "Authorization" : "Bearer {}".format(auth_key)
@@ -45,7 +45,27 @@ def collectTeamGameInfo(teamid, auth_key):
     res = json.loads(req.text)["result"]
     out = []
     for dict in res:
-        out.append(Game(dict.get("HomeTeam").get("name"), dict.get("AwayTeam").get("name"), dict.get("start_datetime_tz")))
+        name = dict.get('name')
+        id = dict.get('id')
+        img = dict.get('images').get('200')
+        start = dict.get('start')
+        out.append(wumodel.Event(id, name, img, start))
+    return out
+
+def collectTeamGames(teamid, auth_key):
+    """Return list of game objects for each upcoming game for teamid"""
+    url = "https://wds.usetopscore.com/api/games?&team_id={}&active_events_only=true&min_date={}".format(teamid,datetime.datetime.now().strftime("%Y/%m/%d"))
+
+    headers = {
+        "Authorization" : "Bearer {}".format(auth_key)
+    }
+    print(playerid)
+    req = requests.get(url, headers=headers)
+    res = json.loads(req.text)["result"]
+    out = []
+    for dict in res:
+        #print("{} vs {}".format(dict.get("HomeTeam").get("name"), dict.get("AwayTeam").get("name")))
+        out.append(wumodel.Game(dict.get("HomeTeam").get("name"), dict.get("AwayTeam").get("name"), dict.get("start_date"), dict.get("start_time")))
     return out
 
 
