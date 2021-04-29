7c340e0cabf7050bb9991cdf12fc292d8b89e866
BuildTools <unconfigured@null.spigotmc.org>
2021-04-29 23:19:53 +1200

    now deletes saved token when reloaded
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- app/src/main/java/com/macneet/wuapp/datamanagers/OAuthManager.java
+++ app/src/main/java/com/macneet/wuapp/datamanagers/OAuthManager.java
@@ -54,6 +54,10 @@ public class OAuthManager extends DataManager {
 
     @Override
     public void reload() {
+        File file = new File(context.getFilesDir(), "token.txt");
+        if(file.exists()) {
+            file.delete();
+        }
         exception = null;
         if(oAuthToken != null) { return; }
         oAuthToken = readToken(context);
